19a89754ca5a99830353ee33024e5b1ecc25e9d9
pescador95 <iedio_junior@hotmail.com>
2022-09-17 20:02:47 -0300

    Efetuado CRUD do cliente

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
   console.log("Hello World");
 });
 
-app.get("/cliente", async (req, res) => {
+app.get("/cliente/", async (req, res) => {
   try {
     const { rows } = await pool.query("SELECT * FROM cliente");
     return res.status(200).send(rows);
@@ -24,17 +24,45 @@ app.get("/cliente", async (req, res) => {
   }
 });
 
-app.post("/cliente", async (req, res) => {
+app.post("/cliente/create", async (req, res) => {
   const { nome, cpf, telefone, datanascimento, endereco } = req.body;
-  let cliente = "";
+  const { id } = req.params;
+
   try {
-    cliente = await pool.query("SELECT * FROM cliente WHERE nome = $1", [nome]);
-    if (!cliente.rows[0]) {
-      cliente = await pool.query(
-        "INSERT INTO cliente (nome, cpf, telefone, datanascimento, endereco) VALUES ($1, $2, $3, $4 ,$5) RETURNING *",
-        [nome, cpf, telefone, datanascimento, endereco]
-      );
-    }
+    const cliente = await pool.query(
+      "INSERT INTO cliente (nome, cpf, telefone, datanascimento, endereco) VALUES ($1, $2, $3, $4 ,$5) RETURNING *",
+      [nome, cpf, telefone, datanascimento, endereco]
+    );
+    console.log("Cliente cadastrado com sucesso!");
+    return res.status(200).send(cliente.rows);
+  } catch (err) {
+    return res.status(400).send(err);
+  }
+});
+
+app.put("/cliente/update", async (req, res) => {
+  const { nome, cpf, telefone, datanascimento, endereco, id } = req.body;
+
+  try {
+    const cliente = await pool.query(
+      "UPDATE cliente SET nome = $1, cpf = $2, telefone = $3, datanascimento = $4, endereco = $5 WHERE id = $6 RETURNING *",
+      [nome, cpf, telefone, datanascimento, endereco, id]
+    );
+    console.log("Cliente atualizado com sucesso!");
+    return res.status(200).send(cliente.rows);
+  } catch (err) {
+    return res.status(400).send(err);
+  }
+});
+
+app.delete("/cliente/delete", async (req, res) => {
+  const { nome } = req.body;
+  const { id } = req.params;
+  try {
+    const cliente = await pool.query("DELETE FROM cliente WHERE nome = $1", [
+      nome,
+    ]);
+    console.log("Cliente deletado com sucesso!");
     return res.status(200).send(cliente.rows);
   } catch (err) {
     return res.status(400).send(err);
@@ -43,4 +71,4 @@ app.post("/cliente", async (req, res) => {
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-//create a insert query to insert a new cliente into the database
+//create a CRUD for the table cliente into the database api_restaurante
